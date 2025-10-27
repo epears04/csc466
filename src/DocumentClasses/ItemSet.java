14a6b7f13dbf55fe8dcf1357420030b84375f16a
Ellie Pearson <ellie.jm.pearson@gmail.com>
2025-10-27 13:38:34 -0700

    find item sets of size 1

--- src/DocumentClasses/ItemSet.java
+++ src/DocumentClasses/ItemSet.java
@@ -1,8 +1,54 @@
 package DocumentClasses;
 
+import java.util.*;
+
 public class ItemSet {
 
-    public ItemSet(String itemSet) {
+    private ArrayList<Integer> items;
+    private int transactionNum;
 
+    public ItemSet(String itemSet, HashMap<Integer, Integer> itemFrequency) {
+        // item set input as transaction number followed by a list of items that were purchases
+        String[] tokens = itemSet.split(", ");
+        transactionNum = Integer.parseInt(tokens[0]);
+        items = new ArrayList<>();
+        // start at 1 to account for transaction number
+        for (int i = 1; i < tokens.length; i++) {
+            int item = Integer.parseInt(tokens[i]);
+            items.add(item);
+            // no duplicate items in transaction so will only add once
+            itemFrequency.merge(item, 1, Integer::sum); // number of times it appears in transaction
+        }
+        // check for duplicate items in a transaction
+        if (new HashSet<>(items).size() != items.size()) {
+            System.out.println("Error: ItemSet contains duplicate items!");
+        }
+    }
+
+    // create an itemSet with a single item
+    public ItemSet(int item) {
+        items = new ArrayList<>(List.of(item));
+        transactionNum = -1; // not associated with transaction
+    }
+
+    public ArrayList<Integer> getItems() {
+        return items;
+    }
+
+    public int getTransactionNum() {
+        return transactionNum;
+    }
+
+    public int getSize() {
+        return items.size();
+    }
+
+    public boolean containsItem(int item) {
+        return items.contains(item);
+    }
+
+    @Override
+    public String toString() {
+        return items.toString();
     }
 }
--- src/labs/Lab5.java
+++ src/labs/Lab5.java
@@ -1,11 +1,74 @@
 package labs;
 
+import DocumentClasses.ItemSet;
+import java.io.*;
 import java.util.*;
 
 public class Lab5 {
 
-//    ArrayList<ItemSet> transactions //lists of all itemsets
-//    ArrayList<Integer> items //lists of all items
-//    HashMap<Integer, ArrayList<ItemSet>> frequentItemSet //lists frequent imtemsets. E.g., for key=1, store all 1-itemsets, for key=2, all 2-itemsets and so on.
+    private static final double minsup = 0.01;
+    private static ArrayList<ItemSet> transactions; //lists of all itemSets
+    private static ArrayList<Integer> items; //lists of all items
 
+    //lists frequent itemSets. E.g., for key=1, store all 1-itemSets, for key=2, all 2-itemSets and so on.
+    private static HashMap<Integer, ArrayList<ItemSet>> frequentItemSet;
+    // key: transaction number, value: number of transactions it is in
+    private static HashMap<Integer, Integer> itemFrequency;
+
+    public static void main() {
+        transactions = new ArrayList<>();
+        items = new ArrayList<>();
+        itemFrequency = new HashMap<>();
+        frequentItemSet = new HashMap<>();
+        process("./files/shopping_data.txt");
+        findFrequentSingleItemSets();
+//        System.out.println(frequentItemSet);
+        for (Map.Entry<Integer, ArrayList<ItemSet>> entry : frequentItemSet.entrySet()) {
+            System.out.println(entry.getKey());
+            System.out.println(entry.getValue());
+        }
+    }
+
+    // process input file and populate transactions and items
+    public static void process(String fileName) {
+        File file = new File(fileName);
+        try(Scanner reader = new Scanner(file)) {
+            while (reader.hasNextLine()) {
+                ItemSet itemSet = new ItemSet(reader.nextLine(), itemFrequency);
+                transactions.add(itemSet);
+                items.addAll(itemSet.getItems());
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        }
+    }
+
+    //finds all k-itemSets, Returns false if no itemSets were found (precondition k>=2)
+    public static boolean findFrequentItemSets(int k) {
+        if (k < 2) {
+            return false;
+        }
+        return k==3;
+    }
+
+    // returns true if itemSet is frequent
+    public static boolean isFrequent(ItemSet itemSet) {
+        int item = itemSet.getItems().getFirst();
+        double freq = (double) itemFrequency.getOrDefault(item, 0) / transactions.size();
+        return freq >= minsup;
+        // TODO: handle k >= 2
+    }
+
+    //finds all 1-itemSets
+    public static void findFrequentSingleItemSets() {
+        ArrayList<ItemSet> singleSets = new ArrayList<>();
+        for (Map.Entry<Integer, Integer> entry : itemFrequency.entrySet()) {
+            double ratio = (double) entry.getValue() / transactions.size();
+            if (ratio >= minsup) {
+                ItemSet itemSet = new ItemSet(entry.getKey());
+                singleSets.add(itemSet);
+            }
+        }
+        frequentItemSet.put(1, singleSets);
+    }
 }
