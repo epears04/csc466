18c834e40c2a496562e2c57913f297a5853c70af
Ellie Pearson <ellie.jm.pearson@gmail.com>
2025-11-13 17:33:33 -0800

    finish lab 7

--- src/DocumentClasses/Matrix.java
+++ src/DocumentClasses/Matrix.java
@@ -13,17 +13,33 @@ public class Matrix {
 
     // returns the number of rows in which the element at position attribute (a number between 0 and 4) is equal to value.
     private int findFrequency(int attr, int value, ArrayList<Integer> rows) {
-        return 0;
+        int freq = 0;
+        for (Integer row : rows) {
+            if (matrix[row][attr] == value) {
+                freq++;
+            }
+        }
+        return freq;
     }
 
     // returns a HashSet of the different values for the specified attribute
     private HashSet<Integer> findDifferentValues(int attr, ArrayList<Integer> rows) {
-        return null;
+        HashSet<Integer> differentValues = new HashSet<>();
+        for (Integer row : rows) {
+            differentValues.add(matrix[row][attr]);
+        }
+        return differentValues;
     }
 
     //  Returns an ArrayList of the rows where the value for the attribute is equal to value
     private ArrayList<Integer> findRows(int attr, int value, ArrayList<Integer> rows) {
-        return null;
+        ArrayList<Integer> foundRows = new ArrayList<>();
+        for (Integer row : rows) {
+            if (matrix[row][attr] == value) {
+                foundRows.add(row);
+            }
+        }
+        return foundRows;
     }
 
     // returns log2 of input
@@ -31,36 +47,93 @@ public class Matrix {
         return Math.log(number) / Math.log(2);
     }
 
-    // finds the entropy of the dataset that consists of the specified rows after it is partitioned on the attribute
+    // finds the entropy of the dataset that consists of the specified rows
     private double findEntropy(ArrayList<Integer> rows) {
-        return 0.0;
+        double entropy = 0.0;
+        // index 0 = class 1, index 1 = class 2, index 2 = class 3
+        int[] counts = new int[3];
+        for (int row : rows) {
+            int classification = matrix[row][matrix[0].length - 1];
+            if (classification >= 1 && classification <= 3) {
+                counts[classification - 1]++;
+            }
+        }
+        if (!rows.isEmpty()) {
+            for (int integer : counts) {
+                double prob = (double) integer / rows.size();
+                if (prob != 0) {
+                    entropy -= prob * log2(prob);
+                }
+            }
+        }
+        return entropy;
     }
 
-    //finds the entropy of the dataset that consists of the specified rows after it is partitioned on the attribute
+    // weighted average entropy of the specified rows after it is partitioned on the attribute
     private double findEntropy(int attribute, ArrayList<Integer> rows) {
-        return 0.0;
+        if (rows.isEmpty()) {
+            return 0.0;
+        }
+        double weightedEntropy = 0.0;
+        HashSet<Integer> values = findDifferentValues(attribute, rows);
+        for (int value : values) {
+            ArrayList<Integer> matchingRows = findRows(attribute, value, rows);
+            double entropy = findEntropy(matchingRows);
+            weightedEntropy += ((double) matchingRows.size() / rows.size()) * entropy;
+        }
+        return weightedEntropy;
     }
 
     // finds the information gain of partitioning on the attribute. Considers only the specified rows.
     private double findGain(int attribute, ArrayList<Integer> rows)  {
-        return 0.0;
+        return findEntropy(rows) -  findEntropy(attribute, rows);
     }
 
     // returns the Information Gain Ratio, where we only look at the data defined by the set of rows and we consider splitting on attribute.
     public double computeIGR(int attribute, ArrayList<Integer> rows) {
-        return 0.0;
+        if (rows.isEmpty()) {
+            return 0.0;
+        }
+        double gain = findGain(attribute, rows);
+        double denominator = 0.0;
+        HashSet<Integer> values = findDifferentValues(attribute, rows);
+        for (int value : values) {
+            double prob = (double) findFrequency(attribute, value, rows) / rows.size();
+            if (prob != 0) {
+                denominator -= prob * log2(prob);
+            }
+        }
+        return gain / denominator;
     }
 
     // returns the most common category for the dataset that is the defined by the specified rows.
     public int findMostCommonValue(ArrayList<Integer> rows) {
-        return 0;
+        int maxCount = -1;
+        int mostCommonClass = -1;
+        HashSet<Integer> classes = findDifferentValues(matrix[0].length-1, rows);
+        for (int c : classes) {
+            int freq = findFrequency(matrix[0].length - 1, c, rows);
+            if (freq > maxCount) {
+                maxCount = freq;
+                mostCommonClass = c;
+            }
+        }
+        return mostCommonClass;
     }
 
-    // Splits the dataset that is defined by rows on the attribute. Each element of the HashMap that is returned contains the value for the attribute and an ArrayList of rows that have this value.
+    // Splits the dataset that is defined by rows on the attribute.
+    // Each element of the HashMap that is returned contains the value for the attribute and an ArrayList of rows that have this value.
     public HashMap<Integer, ArrayList<Integer>> split(int attribute, ArrayList<Integer> rows) {
-        return null;
+        HashMap<Integer, ArrayList<Integer>> result = new HashMap<>();
+        HashSet<Integer> values = findDifferentValues(attribute, rows);
+        for (int value : values) {
+            ArrayList<Integer> matchingRows = findRows(attribute, value, rows);
+            result.put(value, matchingRows);
+        }
+        return result;
     }
 
-
-
+    public boolean meetsThreshold(double threshold, ArrayList<Integer> rows) {
+        return findEntropy(rows) <= threshold;
+    }
 }
--- src/labs/Lab7.java
+++ src/labs/Lab7.java
@@ -6,11 +6,21 @@ import java.util.*;
 
 public class Lab7 {
 
+    private static final double THRESHOLD = 0.01;
     // stores [data][attribute]
     private static int[][] data;
+    private static Matrix matrix;
     public static void main(String[] args) {
         data = process("./files/data.txt");
-        Matrix matrix = new Matrix(data);
+        matrix = new Matrix(data);
+
+        ArrayList<Integer> attributes = new ArrayList<>(List.of(0, 1, 2, 3));
+        ArrayList<Integer> rows = new ArrayList<>();
+        for (int i = 0; i < data.length; i++) {
+            rows.add(i);
+        }
+
+        printDecisionTree(data, attributes, rows, 0, 100);
     }
 
     public static int[][] process(String filename) {
@@ -34,5 +44,54 @@ public class Lab7 {
             System.err.println("File " + filename + " not found.");
         }
         return matrix;
+    }
+
+    // prints tabs according to number of levels
+    private static void printIndents(int level) {
+        for(int i = 0; i < level; i++) {
+            System.out.print("\t");
+        }
+    }
+
+    //recursive method that prints the decision tree.
+    // It takes as input the data, the set of attributes that have not been used so far in this branch of the tree, the set of rows to examine, the current level, and the information gain ratio from last iteration
+    public static void printDecisionTree(int[][] data, ArrayList<Integer> attributes, ArrayList<Integer> rows, int level, double currentIGR) {
+        if (rows.isEmpty() || matrix.meetsThreshold(THRESHOLD, rows)) {
+            int value = matrix.findMostCommonValue(rows);
+            printIndents(level);
+            System.out.println("value = " + value);
+            return;
+        }
+
+        int bestAttr = -1;
+        double bestIGR = Double.NEGATIVE_INFINITY;
+        for (int attr : attributes) {
+            double igr = matrix.computeIGR(attr, rows);
+            if (igr > bestIGR) {
+                bestIGR = igr;
+                bestAttr = attr;
+            }
+        }
+
+        // no split
+        if (bestAttr == -1 || bestIGR <= THRESHOLD) {
+            int value = matrix.findMostCommonValue(rows);
+            printIndents(level);
+            System.out.println("value = " + value);
+            return;
+        }
+
+        // update possible attributes
+        ArrayList<Integer> updatedAttributes = new ArrayList<>(attributes);
+        updatedAttributes.remove(Integer.valueOf(bestAttr));
+
+        HashMap<Integer, ArrayList<Integer>> attributeMap = matrix.split(bestAttr, rows);
+        ArrayList<Integer> values = new ArrayList<>(attributeMap.keySet());
+        for (int v : values) {
+            ArrayList<Integer> childRows = attributeMap.get(v);
+            printIndents(level);
+            System.out.println("When attribute " +(bestAttr + 1)+ " has value " + v);
+            printDecisionTree(data, updatedAttributes, childRows, level + 1, bestIGR);
+        }
     }
 }
